9193ae5637b147c698559ec5c4d430e0d4641b95
Jose Murga <murga.jose16@gmail.com>
2026-02-04 15:45:50 -0500

    feat: init main components

---
+++ src/app/modules/home/home.component.ts
@@ -0,0 +1,12 @@
+import { NgOptimizedImage } from "@angular/common";
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
--- src/app/modules/main.routes.ts
+++ src/app/modules/main.routes.ts
@@ -1,5 +1,25 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from '../layout/layout.component';
+import { AuthGuard } from '../auth/guards/auth.guard';
 
 export const routes: Routes = [
-
+  {
+    path: '',
+    component: LayoutComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./home/home.component').then(m => m.HomeComponent),
+        title: 'Inicio'
+      },
+      
+    ]
+  }
 ];
